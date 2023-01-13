580d81d547cf27117281e735fa0af23a9a06a18c
Oluremi Abraham <olurmeiabraham9@gmail.com>
2023-01-13 18:11:30 +0000

    corr.

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,10 @@
 # SSH client configuration
-exec { 'echo -e "IdentityFile ~/.ssh/school\n" >> /etc/ssh/ssh_config':
-  provider => shell,
-  path     => '/etc/ssh/ssh_config',
-  command  => 'echo -e "PasswordAuthentication no\n" >> /etc/ssh/ssh_config',
+file_line{'Turn off passwd auth':
+path => '/etc/ssh/ssh_config',
+line => 'PasswordAuthentication no'
+}
+
+file_line{'Declare identity file':
+path => '/etc/ssh/ssh_config',
+line => 'IdentityFile ~/.ssh/school'
 }
