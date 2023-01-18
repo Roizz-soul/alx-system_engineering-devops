881956b5e9e5e4fd5dc5d08d90e28e63516c39f9
Oluremi Abraham <olurmeiabraham9@gmail.com>
2023-01-18 18:01:12 +0000

    fix

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,8 @@
+# installing nginx and adding to header using puppet
+exec { 'Nginx install with custom header response':
+command => 'sudo apt-get update -y && sudo apt-get install nginx -y && 
+            header="\\\tadd_header X-Served-By $HOSTNAME;\n" && 
+            sudo sed -i "38i $header" /etc/nginx/sites-available/default && 
+            sudo service nginx restart',
+provider => shell,
+}
