d2a2a21b701a53991ea511c425f5e9a41b177a1c
Oluremi Abraham <olurmeiabraham9@gmail.com>
2023-01-18 18:06:21 +0000

    Task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,9 @@
 # installing nginx and adding to header using puppet
 exec { 'Nginx install with custom header response':
 command => 'sudo apt-get update -y && sudo apt-get install nginx -y && 
-            header="\\\tadd_header X-Served-By $HOSTNAME;\n" && 
-            sudo sed -i "38i $header" /etc/nginx/sites-available/default && 
+            echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html && 
+			header="\\\t\tadd_header X-Served-By $HOSTNAME;" && 
+			sudo sed -i "/^\tlocation.*/a $header" /etc/nginx/sites-available/default && 
             sudo service nginx restart',
 provider => shell,
 }
