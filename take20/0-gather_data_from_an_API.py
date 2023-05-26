fcdc50f20bf31a8e7985168d6f419dbc8d2c7dbf
Oluremi Abraham <olurmeiabraham9@gmail.com>
2023-05-26 01:42:15 +0000

    Work

---
+++ take20/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Requests info from an api and displays the processed content"""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user_s = requests.get(url + "users/{}".format(sys.argv[1]))
+    todo_s = requests.get(url + "todos?userId={}".format(sys.argv[1]))
+
+    i = 0
+    j = 0
+    for data in todo_s.json():
+        j = j + 1
+        if data["completed"] is True:
+            i = i + 1
+
+    print("Employee {} is done with tasks({}/{}):".format(
+                user_s.json()["name"], i, j))
+    for data in todo_s.json():
+        if data["completed"] is True:
+            print("\t {}".format(data["title"]))
---
+++ take20/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Requests info from an api and displays the processed content"""
+import csv
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    uid = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com/'
+    user_s = requests.get(url + 'users/{}'.format(uid))
+    todo_s = requests.get(url + 'todos?userId={}'.format(uid))
+
+    with open('{}.csv'.format(uid), 'w', newline="") as csvf:
+        writer = csv.writer(csvf, quoting=csv.QUOTE_ALL)
+        for data in todo_s.json():
+            row = ['{}'.format(uid),
+                   '{}'.format(user_s.json()["username"]),
+                   '{}'.format(data["completed"]),
+                   '{}'.format(data["title"])]
+            writer.writerow(row)
