de12358c7a6341582fe965730c4eea5eee1d36e0
Oluremi Abraham <oluremiabraham9@gmail.com>
2023-08-08 15:31:02 +0100

    querying subreddit fo top ten hot posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Function to query the Reddit API and returns the top ten hot posts
+for a given subreddit"""
+import requests
+
+
+def top_ten(subreddit):
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    params = {
+        "limit": 10
+    }
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    if response.status_code != 200:
+        print("None")
+        return 0
+    data = response.json()
+    for i in data["data"]["children"]:
+        print(i["data"]["title"])
