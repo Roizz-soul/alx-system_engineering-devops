eff7e33c810e266fbab9a8d024be4f387bf61044
root <root@DESKTOP-KA47RCI>
2023-08-08 14:35:47 +0100

    Querying the subreddit API

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Function to query the Reddit API and returns the number of
+   subscribers for a given subreddit"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, allow_redirects=False)
+    if response.status_code != 200:
+        return "Failed"
+    data = response.json()
+    return (data["data"]["subscribers"])
